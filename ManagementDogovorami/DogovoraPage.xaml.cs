4f78bd1b0dfb997543630b053883d9870670b727
Daniil <i_daniil24@mail.ru>
2022-05-28 02:31:06 +0600

    Добавьте файлы проекта.

---
+++ ManagementDogovorami/DogovoraPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ManagementDogovorami
+{
+    /// <summary>
+    /// Логика взаимодействия для DogovoraPage.xaml
+    /// </summary>
+    public partial class DogovoraPage : Page
+    {
+        public DogovoraPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
